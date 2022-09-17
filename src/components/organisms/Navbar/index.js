8d31884a60c013adb4a507f0dd6e70fdf3b527b2
Tanishq Sangwan <tanishqsangwan7@gmail.com>
2022-09-17 15:06:35 +0530

    add a mobile sidebar

--- src/components/organisms/Navbar/index.js
+++ src/components/organisms/Navbar/index.js
@@ -1,4 +1,5 @@
 import { AppBar, Button, Toolbar } from "@mui/material";
+import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -6,12 +7,13 @@ import UserHelper from "../../../helper/UserHelper";
 import { useMobileView } from "../../../utils/useMobileView";
 import { FlexBox } from "../../atoms";
 import { AuthButtons } from "../../molecules";
+import navbarItems from "./NavbarItems";
 import styles from "./styles.module.scss";
 
-
 const Navbar = ({logoOnly}) => {
+  const [sidenavOpen, setSidenavOpen] = useState(false)
   const [logoSize, setLogoSize] = useState(50);
-  const isMobile = useMobileView(600);
+  const isMobile = useMobileView(768);
   const [user, setUser] = useState(null)
   
   useEffect(() => {
@@ -33,14 +35,71 @@ const Navbar = ({logoOnly}) => {
     >
       <Toolbar disableGutters className={styles.navbarInner}>
         <FlexBox fullWidth align justifyBetween className={styles.navbarInner}>
-          <Link href="/" className={styles.navbarLogo}>
-            <FlexBox align style={{cursor: "pointer"}}>
-              <Image src="/logo.png" height={logoSize} width={logoSize} alt={"Brand Logo"}/>
-              {!isMobile && (
-                <h2>Hoopla</h2>
-              )}
+          <FlexBox align>
+            {isMobile && (
+              <FlexBox column align justify className={styles.hamburgerContainer} onClick={() => setSidenavOpen(!sidenavOpen)}>
+                <div className={styles.hamburger} />
+              </FlexBox>
+            )}
+            <Link href="/">
+              <FlexBox align justify style={{cursor: "pointer", marginRight: '1rem'}} className={styles.navbarLogo}>
+                <Image src="/logo.png" height={logoSize} width={logoSize} alt={"Brand Logo"}/>
+                {!isMobile && (
+                  <h2>Hoopla</h2>
+                )}
+              </FlexBox>
+            </Link>
+            {!isMobile && (
+              <>
+                {navbarItems.map(item => {
+                  if (!item.mobileOnly) {
+                    return (
+                      <Link href={`/${item.to}`}>
+                        <Button className={styles.navButtonDesktop}>{item.title}</Button>
+                      </Link>
+                    )
+                  }
+                })}
+              </>
+            )}
+          </FlexBox>
+  
+          {isMobile && (
+            <FlexBox column className={clsx(styles.mobileSidebar, sidenavOpen && styles.mobileSidebarOpen)}>
+              <FlexBox align justifyBetween style={{marginBottom: '1rem'}}>
+                <Link href="/">
+                  <FlexBox align style={{cursor: "pointer", marginRight: '1rem'}} className={styles.navbarLogo}>
+                    <Image src="/logo.png" height={logoSize} width={logoSize} alt={"Brand Logo"}/>
+                    <h2>Hoopla</h2>
+                  </FlexBox>
+                </Link>
+  
+                <FlexBox column align justify className={styles.hamburgerContainer} onClick={() => setSidenavOpen(!sidenavOpen)}>
+                  <div className={styles.hamburger} />
+                </FlexBox>
+              </FlexBox>
+              
+  
+              {navbarItems.map(item => {
+                if (!item.mobileOnly) {
+                  return (
+                    <Link href={`/${item.to}`}>
+                      <FlexBox align className={styles.navButtonMobile}>
+                        <FlexBox align justify className={styles.icon}>
+                          <item.icon />
+                        </FlexBox>
+                        {item.title}
+                      </FlexBox>
+                    </Link>
+                  )
+                }
+              })}
+              
+              <FlexBox align justifyBetween style={{padding: '1rem 1.5rem'}}>
+                <AuthButtons />
+              </FlexBox>
             </FlexBox>
-          </Link>
+          )}
           
           {!logoOnly && (
             <div>
